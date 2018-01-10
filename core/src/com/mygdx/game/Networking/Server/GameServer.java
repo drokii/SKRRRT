842912fc863c0efdc3d68e1dab170e94b8bf8c64
HIGHPOWEREDTOAS\Pedro <pedrodossantosmarques@gmail.com>
2018-01-10 13:23:05 +0100

    Game server work

--- core/src/com/mygdx/game/Networking/Server/GameServer.java
+++ core/src/com/mygdx/game/Networking/Server/GameServer.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.Networking.Server;
 
+import com.badlogic.gdx.math.Vector2;
 import com.esotericsoftware.kryo.Kryo;
 import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
@@ -8,19 +9,22 @@ import com.mygdx.game.Networking.Lobby;
 import com.mygdx.game.Networking.Network;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class GameServer {
 
     private static Server server;
-    private static Kryo kryo;
-    private List<PlayerInstance> players;
-    private List<Lobby> lobbies;
-    private int lobbySize = 2; //to be substituted by lobby
+    private List<String> players;
+    private Map<String, Velocities> velocityMap;
+
 
 
     public GameServer() throws IOException {
 
+        players = new ArrayList<String>();
         server = new Server();
         server.start();
         server.bind(54555, 54777);
@@ -37,18 +41,69 @@ public class GameServer {
     private void addListenersToServer(final Server server) {
         server.addListener(new Listener() {
             public void received(Connection connection, Object object) {
+                /*
+                If a gamestart request is recieved, the sending player is added to the playerlist
+                If the playerlist contains 2 players, the gamestart response is sent with
+                a map containing players and their spawns.
+                */
                 if (object instanceof Network.GameStartRequest) {
 
+                    players.add(((Network.GameStartRequest) object).nickname);
+                    if(players.size() <= 2){
+                        server.sendToAllTCP(generateGameStartResponse(players));
+                        velocityMap = new HashMap<String, Velocities>();
 
+                        for (String player :
+                                players) {
+                            velocityMap.put(player, new Velocities(new Vector2(0,0),0f));
+                        }
+                    }
 
                 }
+                /*
+                If a gameUpdateRequest is recieved, the server updates the local velocity map and sends the
+                sending player the updated map with the speeds of the other players.
+                */
                 if (object instanceof Network.GameUpdateRequest) {
 
-                    connection.sendTCP(players);
+                    String nickname = ((Network.GameUpdateRequest) object).nickname;
+                    Vector2 linearSpeed = ((Network.GameUpdateRequest) object).speed;
+                    float angularSpeed = ((Network.GameUpdateRequest) object).angularSpeed;
+                    velocityMap.put(nickname, new Velocities(linearSpeed, angularSpeed));
+
+                    Network.GameUpdateResponse gameUpdateResponse = new Network.GameUpdateResponse();
+                    gameUpdateResponse.movementVectors = velocityMap;
+
+                    connection.sendTCP(gameUpdateResponse);
 
                 }
             }
         });
+    }
+
+    private Network.GameStartResponse generateGameStartResponse(List<String> names){
+
+        Network.GameStartResponse nsr = new Network.GameStartResponse();
+        Vector2 v1 = new Vector2(1700, 600);
+        Vector2 v2 = new Vector2(1700, 630);
+        Vector2 v3 = new Vector2(1700, 660);
+        Vector2 v4 = new Vector2(1700, 690);
+
+        List<Vector2> vector2List = new ArrayList<Vector2>();
+        vector2List.add(v1);
+        vector2List.add(v2);
+        vector2List.add(v3);
+        vector2List.add(v4);
+
+        Map<Vector2, String> startPositions = new HashMap<Vector2, String>();
+        int i = 0;
+        for (String p :
+                players) {
+            startPositions.put(vector2List.get(i), p);
+            i++;
+        }
+        nsr.startPositions = startPositions ;
+        return nsr;
     }
 }
 
--- core/src/com/mygdx/game/Networking/Server/Velocities.java
+++ core/src/com/mygdx/game/Networking/Server/Velocities.java
@@ -1,4 +1,25 @@
 package com.mygdx.game.Networking.Server;
 
+import com.badlogic.gdx.math.Vector2;
+
 public class Velocities {
+
+
+    private Vector2 linear;
+    private float angular;
+
+    public Velocities(Vector2 linear, float angular) {
+        this.linear = linear;
+        this.angular = angular;
+    }
+
+    public Vector2 getLinear() {
+        return linear;
+    }
+
+    public float getAngular() {
+        return angular;
+    }
+
+
 }
